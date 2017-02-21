e57fbc421ab5ce4633aeb5ef3bf7f6e8b8c79432
Michael Messmer <preactive@gmail.com>
2017-02-20 23:34:12 -0800

    refactor
    removed odd break and looping of object as it was being nullified by the break.

--- OnEditSort.js
+++ OnEditSort.js
@@ -1,25 +1,26 @@
-var sheetToSort = "МОНТАЖ 2017"; // replace this with "Form Responses 1" or whichever sheet you want to sort automatically
-var rangeToSort = "A3:AB30";
-var sortOrder = [{column: 1, ascending: true}];//, // column A = 1, B = 2, etc.
-                 //{column: 3, ascending: true}, // for each column, use true for ascending and false for descending order
-                 //{column: 4, ascending: true},
-                 //{column: 5, ascending: true}];
-
-
-function onEdit() {
+function onEdit()
+{
+  //Scope Limits------------
+  var sheetToSort = ["Sheet8","Sheet13"]; // replace this with "Form Responses 1" or whichever sheet you want to sort automatically
+  var rangeToSort = "A2:AB";
+  var sortOrder = [{column: 1, ascending: true}];//, // column A = 1, B = 2, etc
+  //=======================
   var sheet = SpreadsheetApp.getActiveSheet();
-  if (sheet.getName() != sheetToSort) return;
+  if (sheetToSort.indexOf(sheet.getName()) == -1){return;}
+  
   var editedColumn = sheet.getActiveCell().getColumn();
-  for (var i = 0, numSortColumns = sortOrder.length; i < numSortColumns; i++) {
-    if (sortOrder[i].column == editedColumn) {
-      sortSheet();
-      break;
-    }
+  if(sortOrder[i].column == editedColumn)
+  {
+    sortSheet(sheetToSort,rangeToSort,sortOrder);
   }
 }
 
-function sortSheet() {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetToSort);
+function sortSheet(sheetToSort,rangeToSort,sortOrder)
+{
+  for(var i = 0, iL = sheetToSort.length; i<iL; i++)
+  {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetToSort[i]);
   var range = sheet.getRange(rangeToSort);
   range.sort(sortOrder);
+  }
 }
