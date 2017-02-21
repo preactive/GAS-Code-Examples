451d1f34be14cdd326a8ee54cadd6644ff696ff1
Michael Messmer <preactive@gmail.com>
2017-02-20 19:52:45 -0800

    more notes and sn found column assignments
    Also found for loop now loops the length of the SN array.

--- Sheet_to_Sheet_Alt.js
+++ Sheet_to_Sheet_Alt.js
@@ -9,16 +9,18 @@ Form response:
 
 
 Inventory before:
-| Whse        | SN 1  |    | SN 2  |    | SN 3  |    |
-| 4321        | sdfaw |  x | ngh5c |  x | 4g3qz |  x |
-| 1234        | fftt1 |    | d2h26 |    | bbxx1 |    |
+| Whse        | SN 1  | SN 1 x | SN 2  | SN 2 x | SN 3  | SN 3 x |
+| 4321        | sdfaw |    x   | ngh5c |    x   | 4g3qz |    x   |
+| 1234        | fftt1 |        | d2h26 |        | bbxx1 |        |
 
 Inventory after:
-| Whse        | SN 1  |    | SN 2  |    | SN 3  |    |
-| 4321        | sdfaw |  x | 4g3qz |  x | ngh5c |  x |
-| 1234        | fftt1 |  x | d2h26 |    | bbxx1 |  x |
+| Whse        | SN 1  | SN 1 x | SN 2  | SN 2 x | SN 3  | SN 3 x |
+| 4321        | sdfaw |    x   | 4g3qz |    x   | ngh5c |    x   |
+| 1234        | fftt1 |    x   | d2h26 |        | bbxx1 |    x   |
 */
 // Checking multiple columns to multiple columns across sheets
+// gco_ allows you to find columns by title on a defined row which default to zero if not defined.
+// if a user moved column 'SN 3' to Column A it nothing would change in the code as it is still labeled correctly.
 
 
 function Checker() 
@@ -53,21 +55,26 @@ function Checker()
 
   
   var RColumnsRef   = ['SN 1','SN 2','SN 3','SN 4','SN 5','SN 6','SN 7','SN 8','SN 9','SN 10'];
+  var RColumnsRefX   = ['SN 1 x','SN 2 x','SN 3 x','SN 4 x','SN 5 x','SN 6 x','SN 7 x','SN 8 x','SN 9 x','SN 10 x'];
+
   var ROtherColuns  = ['Whse'];
   
   var RCols      =  gco_(RColumnsRef,Rsheet);
+  var RColsX     =  gco_(RColumnsRefX,Rsheet);
+    
   var ROCols     =  gco_(ROtherColuns,Rsheet);
+    
     
   for (var l = 1, rdL=RecieveData.length; l<rdL; l++)
   {
     if(RecieveData[l][ROCols['Whse']] in FormResponseObj)
     {
-      for(var o = 1; o<3;o++)
+      for(var o = 1, oL = RColumnsRef.length; o<oL; o++)
       {
         if(FormResponseObj[RecieveData[l][ROCols['Whse']]].indexOf(RecieveData[l][RCols['SN ' + o]]) > -1) 
         { 
           if(isEmpty_(RecieveData[l][RCols['SN ' + o]])){continue;}
-          Rsheet.getRange(l+1, RCols['SN ' + o]+2).setValue("X") 
+          Rsheet.getRange(l+1, RColsX['SN ' + o + " x"]+ 1).setValue("Found") 
         }
       }
     } 
